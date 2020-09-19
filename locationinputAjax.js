aeddaf468153f525edd35dda90958341224c7200
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-09-19 12:07:35 -0400

    updated display

--- locationinputAjax.js
+++ locationinputAjax.js
@@ -1,67 +1,67 @@
 // $(document).ready(function () {
 
-    // create/listen to click event for state search and saved history
-    // $("#dropdownMenuButton").on("click",  getLocation);
+// create/listen to click event for state search and saved history
+// $("#dropdownMenuButton").on("click",  getLocation);
 
-    // //$(".history").on("li", function () {
-    //     // return createSavedLocation();
+// //$(".history").on("li", function () {
+//     // return createSavedLocation();
 
-    // });
+// });
 
-    function getStateData(state) {
-        console.log(state)
-        // create a var and assign the value received back from API
-      
-        // var state = $("#searchbar").val();
+function getStateData(state) {
+    console.log(state)
+    // create a var and assign the value received back from API
 
-        // make a request to API openweahtermap
-        
-        $.ajax({
-            url: 'https://api.covidtracking.com/v1/states/'+ state + '/current.json',
-            method: "GET",
-            
-        })
+    // var state = $("#searchbar").val();
 
+    // make a request to API openweahtermap
 
-            // After the data from AJAX comes back, we show the weather
-            .then(function (data) {
+    $.ajax({
+        url: 'https://api.covidtracking.com/v1/states/' + state + '/current.json',
+        method: "GET",
 
-                //create history link for this search
-                // if (history.indexOf(state) === -1) {
-                //     history.push(state);
-                //     window.localStorage.setItem("history", JSON.stringify(history));
-
-                //     createSavedLocation(state);
-                // }
-
-                // clear any old results
-                $("#totalCases").empty();
-                $("#newCases").empty();
-                $("#recovery").empty();
-                $("#totalDeaths").empty();
-                $("#newDeaths").empty();
+    })
 
 
-                // create html content for resutls
-                var totalPositive = $("#totalCases").text("Total cases: " + data.positive);
-                var newPositive = $("#newCases").text("New Cases: " + data.positiveIncrease);
-                
-                var recovery = $("#recovery").text("Recovered: " + data.recovered);
-                //$("newRecovery").text("")
+        // After the data from AJAX comes back, we show the weather
+        .then(function (data) {
 
-                var deaths = $("#totalDeaths").text("Deaths: " + data.death);
-                var newDeaths = $("#newDeaths").text("Death Increase: " + data.deathIncrease);
+            //create history link for this search
+            // if (history.indexOf(state) === -1) {
+            //     history.push(state);
+            //     window.localStorage.setItem("history", JSON.stringify(history));
 
-                // add to the page
-                $("#totalCases").append(totalPositive, newPositive, recovery, deaths, newDeaths);
-           
+            //     createSavedLocation(state);
+            // }
 
-        
+            // clear any old results
+            $("#totalCases").empty();
+            $("#newCases").empty();
+            $("#recovery").empty();
+            $("#totalDeaths").empty();
+            $("#newDeaths").empty();
 
 
-            });
+            // create html content for resutls
+            var totalPositive = $("#totalCases").text(data.positive);
+            var newPositive = $("#newCases").text(`+ ${data.positiveIncrease}`);
 
-    }
+            var recovery = $("#recovery").text(data.recovered);
+            //$("newRecovery").text("")
+
+            var deaths = $("#totalDeaths").text(data.death);
+            var newDeaths = $("#newDeaths").text(`+ ${data.deathIncrease}`);
+
+            // add to the page
+            // $("#totalCases").append(totalPositive, newPositive, recovery, deaths, newDeaths);
+
+
+
+
+
+        });
+
+}
 
     // get current histor if any
     // var history = JSON.parse(window.localStorage.getItem("history")) || [];
