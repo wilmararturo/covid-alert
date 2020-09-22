65f67d6ee5339294b7189ebbdf01c6bd6215e71c
Yerson Arvelo <yerson.a06@gmail.com>
2020-09-21 20:38:12 -0400

    more styling and chart draft

--- savedLocations.js
+++ savedLocations.js
@@ -15,6 +15,7 @@ function savedLocation(state) {
     }
 
     createSavedButton();
+    displayChart();
     // then create a button on the index page with the state info
 } 
 
@@ -29,7 +30,7 @@ function createSavedButton() {
         }
 
     for (var i = 0; i < resultCount; i++) {
-        
+
         var covidApiUrl = 'https://api.covidtracking.com/v1/states/' + history[i] + '/current.json';
 
         $.ajax({
@@ -37,45 +38,62 @@ function createSavedButton() {
         method: "GET"
         }).then(function (data) {
 
-        var newSavedLocation = $("<div>").addClass("card mt-4 ml-4 mr-4 mb-2").text(history[i]);
+        var newSavedLocation = $("<div>").addClass("card mt-4 ml-4 mr-4 mb-2 bg-light").text(history[i]);
         $("#saved-locations").append(newSavedLocation);
         
 
-        var body = $("<div>").addClass("card-body");
-        var title = $("<h3>").addClass("card-title").text(data.state);
-        // ADD CHART IMAGE VISUAL FROM SLACK LINK
-        var positive = $("<p>").addClass("card-text").text("Positive: " + data.positive);
-        var deaths = $("<p>").addClass("card-text").text("Total Deaths: " + data.death);
-        var deathIncrease = $("<p>").addClass("card-text").text("Death Increase: " + data.deathIncrease);
-        var positiveIncrease = $("<p>").addClass("card-text").text("Positive Increased: " + data.positiveIncrease + "+");
-        var recovered = $("<p>").addClass("card-text").text("Recovered: " + data.recovered);
-            
+        var body = $("<div>").addClass("card-body bg-dark float-left");
+        var title = $("<h3>").addClass("card-title text-secondary text-light").text(data.state);
+        var positive = $("<p>").addClass("card-text text-warning").text("Positive: " + data.positive);
+        var deaths = $("<p>").addClass("card-text text-danger").text("Total Deaths: " + data.death);
+        var deathIncrease = $("<p>").addClass("card-text text-danger").text("Death Increase: " + data.deathIncrease);
+        var positiveIncrease = $("<p>").addClass("card-text text-warning").text("Positive Increased: " + data.positiveIncrease + "+");
+        var recovered = $("<p>").addClass("card-text text-success").text("Recovered: " + data.recovered);
+        
         newSavedLocation.append(body);
-        body.append(title, deaths, deathIncrease, recovered, positive, positiveIncrease);
+        body.append(title, deaths, deathIncrease, positive, positiveIncrease, recovered);
+        
+        
+        
         
         })
         }
 }
 
+function displayChart() {
+    // ADD A CHART/IMAGE/VISUAL
+        // might need to use historical daily api to compare numbers month over month
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: ['State1', 'State2', 'State3', 'State4', 'State5'],
+        datasets: [{
+            label: '# of Cases',
+            data: [2, 1, 3, 3, 2],
+            backgroundColor: [
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                
+            ],
+            borderWidth: 3
+        }]
+        },
+        options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+        }
+        });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// // local storage example from weather app that stores searched cities
-
-//     for (var i = 0; i < localStorage.length; i++) {
-//         // local storage stores the input provided by the for loop
-//         var city = localStorage.getItem(i);
-//         // creating a variable to store the city name and adding a new class to display it to screen
-//         var cityName = $(".list-group").addClass("list-group-item");
-//         // append the city input to the screen within the ul
-//         cityName.append("<li>" + city + "</li>");
-//     }
